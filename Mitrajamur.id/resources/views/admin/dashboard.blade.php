d5c8cb07bb07407e6dced9000ac8d08e2d5a7cf2
Eka Setyaningrum <ekasetyaningrum14@gmail.com>
2022-04-04 09:18:14 +0700

    Dashboard Admin Front End

--- Mitrajamur.id/resources/views/admin/dashboard.blade.php
+++ Mitrajamur.id/resources/views/admin/dashboard.blade.php
@@ -1,25 +1,138 @@
-@include('partials.head')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	<link rel="stylesheet" type="text/css" href="{!! asset('assets/css/style.css') !!}">
+    <title>MitraJamur.id</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark shadow sticky-top " style="background-color: #519259;">
+    <div class="container-fluid" style="margin-left: 20px; margin-right: 10px;">
+        <a class="navbar-brand" href="/" style="font-size:1.7rem;">MitraJamur.id</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
-<body class="sb-nav-fixed">
-    <nav class="navbar navbar-expand-lg navbar-dark shadow sticky-top " style="background-color: #519259;">
-        <div class="container-fluid ">
-            <a class="navbar-brand ps-5" href="/admin" style="font-size:20px;">MitraJamur.id</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="nav-r gx-6 " id="navbarNav">
-            <ul class="nav justify-content-end nav-pills nav-justified">
-                <li class="nav-item dropdown" style="padding-right: 15px;">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span><svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="white" class="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/></svg></span>
-                    <ul class="dropdown-menu dropdown-menu-end " aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-            </div>
+        <div class="nav-r gx-6" id="navbarNav">
+        <ul class="nav justify-content-end nav-pills nav-justified">
+            <li class=" nav-item gx-6">
+                <a class="nav-link active rounded" href="/" style="background-color: #519259; font-size:20px;">Home</a>
+            </li>
+        </ul>
+        
         </div>
-    </nav>
-    @include('partials.sidenav')
+    </div>
+</nav>
+<div class="wrapper">
+	
+    <nav id="sidebar">
+        
+ 	    <ul class="lisst-unstyled components">
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse"aria-expanded="false" class="dropdown-toggle">Home</a>
+                <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Product</a>
+                    </li>
+                    <li>
+                        <a href="#">Workshop</a>
+                    </li>
+
+                </ul>
+            </li>
+            <li>
+ 	 		<a href="#">Laporan</a>
+ 	 	</li>
+ 	 	<li>
+ 	 		<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+ 	 		<ul class="collapse list-unstyled" id="pageSubmenu">
+ 	 			<li>
+ 	 				<a href="#">Page 1</a>
+ 	 			</li>
+ 	 			<li>
+ 	 				<a href="#">Page 2</a>
+ 	 			</li>
+ 	 		</ul>
+ 	 	</li>
+
+ 	 	 	 	<li>
+ 	 				<a href="#">Profile</a>
+ 	 			</li>
+        <hr class="soft my-1 bg-white">
+        <li>
+            <a class="nav-link px-3" href="#">
+                <i class="bi bi-box-arrow-right" aria-hidden="true"></i>  LogOut
+            </a>
+ 	 	</li>
+
+ 	 </ul>
+ </nav>
+
+
+<div id="content">
+	
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  	<div class="container-fluid">
+  		<button type="button" id="sidebarCollapse" class="btn  btn-success">
+  			<i class="fas fa-align-left"></i>
+  			<span>CONTOH AJA</span>
+  			
+  		</button>
+  	</div>
+  	  </nav>
+
+<br><br>
+    <h2>Gatau Mau Diisi Apa</h2>
+    <p>
+    	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam asperiores unde quisquam dicta at animi consequuntur soluta fugit ea odit, nihil, saepe ad suscipit exercitationem, ipsum laborum repellat temporibus mollitia? Ex ea minima aliquid quos, delectus hic perferendis cupiditate ab obcaecati voluptatum officiis quis quibusdam esse. Perferendis et ipsum voluptas? Doloremque vero fuga fugit, nulla ipsa, odit officia, cumque deleniti minima adipisci itaque fugiat ipsam? Fugit minus autem, praesentium exercitationem suscipit blanditiis et inventore voluptate quos amet molestiae ullam eum aliquam cum eius vitae delectus. Nihil rem, doloremque eius itaque, minima nostrum ea similique nulla ducimus, odio maxime numquam repudiandae?
+    	
+    </p>
+     <p>
+    	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat asperiores dolorem illo minus, optio nesciunt deleniti ipsa, rem aliquid aspernatur obcaecati aut sunt libero dolore, sint exercitationem! Dolorem ducimus repellendus, quos laboriosam obcaecati repellat facere dolores esse quibusdam? Saepe, quia fuga tempora impedit voluptate assumenda error numquam voluptates corporis rem eius earum, nostrum qui odio officiis delectus? Sunt explicabo aliquam minima accusantium et obcaecati quia, quisquam optio modi, quo facere. Vel illo perferendis eveniet cupiditate quia, officiis sunt numquam beatae sed velit, quasi ad consequuntur commodi nostrum libero modi earum similique dignissimos? Iure sed molestiae praesentium quis, rerum officia debitis!
+    	
+    </p>
+
+  <div class="line"></div>
+  <h3>Nunggu Desain</h3>
+
+   <p>
+    	Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi soluta velit et voluptatibus rerum vitae, quaerat fugit quos mollitia nulla cupiditate quibusdam explicabo temporibus, alias officiis perspiciatis cum odit enim placeat consequatur in eligendi labore. At, perspiciatis? Aperiam, id, voluptate ipsum, fuga pariatur obcaecati officia ipsam quisquam excepturi a tempore numquam magni velit eveniet repudiandae molestias perspiciatis non possimus culpa natus expedita! Praesentium suscipit distinctio ex quo modi, quidem inventore, vel quia eligendi fugit aliquid nobis ea sunt in nihil repellat enim labore ducimus voluptates quos ut quae nesciunt vero quasi! Eligendi corrupti consequuntur in expedita ducimus temporibus, omnis sunt.
+    </p>
+
+
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- <script>
+
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+
+</script> -->
+
+
 </body>
+</html>
+</body>
+</html>
--- Mitrajamur.id/resources/views/layout/main.blade.php
+++ Mitrajamur.id/resources/views/layout/main.blade.php
@@ -2,7 +2,7 @@
 
 <body style="background-color: #D3E4CD; font-family:BR Hendrix Semi Bold;">
 
-    @include('partials.navbar');
+    @include('partials.navbar')
 
     <div class="container-fluid">
         @yield('container-fluid')
